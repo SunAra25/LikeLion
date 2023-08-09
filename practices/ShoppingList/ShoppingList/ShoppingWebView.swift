8da84c9f49b81b9080296102a064fd3fca8ac4be
SunAra25 <tjsdkfk77@naver.com>
2023-08-09 11:45:11 +0900

    WebView 연습과 API 통신 연습

---
+++ practices/ShoppingList/ShoppingList/ShoppingWebView.swift
@@ -0,0 +1,20 @@
+//
+//  ShoppingWebView.swift
+//  ShoppingList
+//
+//  Created by 아라 on 2023/08/04.
+//
+
+import SwiftUI
+
+struct ShoppingWebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShoppingWebView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShoppingWebView()
+    }
+}
